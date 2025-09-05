3e4cc08611a00da492304a80f6afcb67a7e4fe87
mufies <69742343+mufies@users.noreply.github.com>
2025-09-05 15:50:43 +0700

    Delete kita/Controllers/User directory

--- kita/Controllers/User/PlaylistController.cs
+++
@@ -1,9 +0,0 @@
-﻿// ...existing code...
-[HttpPost]
-public IActionResult CreatePlaylist([FromBody] Playlist playlist)
-{
-    var createdPlaylist = _playlistRepository.CreatePlaylist(playlist);
-    return CreatedAtAction(nameof(GetPlaylists), new { id = createdPlaylist.Id }, createdPlaylist);
-}
-// ...existing code...
-
